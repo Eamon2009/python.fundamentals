e7f7cd350d688506ec98d344649e374ab1548a85
Eamon2009 <eamon112009@gmail.com>
2026-01-24 22:03:08 +0530

    dictionary code
    basic dictionary code

---
+++ Dictionary/03-code.py
@@ -0,0 +1,16 @@
+phone=dict()
+i=1
+n=int(input("Enter number of enteries:"))
+while i<=n:
+    a=input("Enter Name:")
+    b=input("Enter phone no:")
+    phone[a]=b
+    i=i+1
+l=phone.key()
+x=int(input("Enter the name to be searched:"))
+for i in l:
+    if i==x:
+        print(x,": phone no =",phone[i])
+        break
+    else:
+        print(x,"not exist......")
